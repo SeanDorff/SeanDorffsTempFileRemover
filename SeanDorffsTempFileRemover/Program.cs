43d260f9619ad334237c46c62067e0ea1d7a4a2b
SeanDorff <seandorff@googlemail.com>
2021-07-11 18:49:21 +0200

    Added request for keypress before closing the application window.

--- SeanDorffsTempFileRemover/Program.cs
+++ SeanDorffsTempFileRemover/Program.cs
@@ -8,6 +8,8 @@ namespace SeanDorffsTempFileRemover
         static void Main(string[] args)
         {
             new DirectoryCleaner();
+            ConsoleWriter.WriteLine("Press any key to close this window.");
+            Console.ReadKey();
         }
     }
 }
