27bbc0477a1c00ad04af2651503b485d26ab4594
SeanDorff <seandorff@googlemail.com>
2021-07-13 15:57:02 +0200

    Added another layer of parallelism

--- SeanDorffsTempFileRemover/DirectoryCleaner.cs
+++ SeanDorffsTempFileRemover/DirectoryCleaner.cs
@@ -22,19 +22,23 @@ namespace SeanDorffsTempFileRemover
         private void DeleteDirectoryContents(string directory)
         {
             List<string> subDirectories = new List<string>(Directory.GetDirectories(directory));
-            foreach (string subDirectory in subDirectories)
+
+            switch (subDirectories.Count)
             {
-                DeleteDirectoryContents(subDirectory);
-                ConsoleWriter.WriteLine("\t\tDirectory: " + subDirectory);
-                try
-                {
-                    Directory.Delete(subDirectory);
-                    ConsoleWriter.WriteLine("\t\t\t...deleted");
-                }
-                catch (IOException e)
-                {
-                    ConsoleWriter.WriteLine("\t\t\t" + e.Message);
-                }
+                case 0:
+                    break;
+                case 1:
+                    DeleteSubDirectory(subDirectories[0]);
+                    break;
+                default: // > 1
+                    Task[] taskArray = new Task[subDirectories.Count];
+                    int taskNo = 0;
+                    foreach (string subDirectory in subDirectories)
+                    {
+                        taskArray[taskNo++] = Task.Factory.StartNew(() => { DeleteSubDirectory(subDirectory); });
+                    }
+                    Task.WaitAll(taskArray);
+                    break;
             }
 
             List<string> files = new List<string>(Directory.GetFiles(directory));
@@ -54,6 +58,21 @@ namespace SeanDorffsTempFileRemover
                 {
                     ConsoleWriter.WriteLine("\t\t\t" + e.Message);
                 }
+            }
+        }
+
+        private void DeleteSubDirectory(string subDirectory)
+        {
+            DeleteDirectoryContents(subDirectory);
+            ConsoleWriter.WriteLine("\t\tDirectory: " + subDirectory);
+            try
+            {
+                Directory.Delete(subDirectory);
+                ConsoleWriter.WriteLine("\t\t\t...deleted");
+            }
+            catch (IOException e)
+            {
+                ConsoleWriter.WriteLine("\t\t\t" + e.Message);
             }
         }
     }
